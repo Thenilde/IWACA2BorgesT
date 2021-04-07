51bd2d1a67a267a62311ec2916891de814ae6cf8
Thenikde <thenilde.tb@hotmail.com>
2021-04-07 00:14:04 +0000

    change4

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 require('./models/db');
 
 const express=require('express');
+const path=require('path');
+const exphbs=require('express-handlebars');
+
+const musicController=require('./controllers/musicController');
+
 var app=express();
+
+app.set('views' ,path.join(__dirname, '/views'));
+//I stopped here
 
 app.listen(3000, ()=>{
     console.log('Express server start at port :3000');
 });
+
+app.use('/music',musicController);
