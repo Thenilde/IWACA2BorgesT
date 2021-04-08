744b3866764f7e68f5c5fd1e01ab8546eaaf802c
Thenikde <thenilde.tb@hotmail.com>
2021-04-08 01:30:03 +0000

    changes11

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -2,7 +2,13 @@ const express=require('express');
 var router=express.Router();
 
 router.get('/',(req, res)=>{
-    res.json('sample sample');
+    res.render("music/addOrEdit",{
+        viewTitle:"Welcome to the instruments store B"
+    });
 });
+
+router.post('/',(req, res)=>{
+     console.log('hi');
+    });
 
 module.exports=router;
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ require('./models/db');
 const express=require('express');
 const path=require('path');
 const exphbs=require('express-handlebars');
+const bodyparser=require('body-parser');
 
 const musicController=require('./controllers/musicController');
 
 var app=express();
+app.use(bodyparser.urlencoded({
+    extended:true
+}));
 
+app.use(bodyparser.json());
 app.set('views' ,path.join(__dirname, '/views'));
 app.engine('hbs',exphbs({extname: 'hbs',defaultLayout:'mainLayout',layoutDir:__dirname + '/views/layout'}));
 app.set('view engine', 'hbs');
