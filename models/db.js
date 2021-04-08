d2b2aa83187288edd9933b4ccaee759db5641c0a
Thenikde <thenilde.tb@hotmail.com>
2021-04-08 18:31:06 +0000

    changes21

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb+srv://Music:123@cluster0.vkjg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://music:123@cluster0.vkjg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 { useNewUrlParser:true},(err)=>{
    
     if (!err){ console.log('Congratulations your mongo is connected')}
