246aadde469214923303e76f2676d175bcc2e44e
Thenikde <thenilde.tb@hotmail.com>
2021-04-09 23:42:08 +0000

    changes34

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 
-mongoose.connect('mongodb+srv://music:123@cluster0.xnrvy.mongodb.net/Musical_Work?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://store:123@cluster0.xnrvy.mongodb.net/Musical_Work?retryWrites=true&w=majority',
 { useNewUrlParser:true},(err)=>{
     if (!err){ console.log('Congratulations your mongo is connected TB')}
     else{console.log('Error in your DB Borges:' + err)}
