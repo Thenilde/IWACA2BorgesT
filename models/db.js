ee5c513f027c88c11126113515a613df4ed353ec
Thenikde <thenilde.tb@hotmail.com>
2021-04-09 01:30:58 +0000

    changes15

--- models/db.js
+++ models/db.js
@@ -2,14 +2,22 @@ const mongoose=require('mongoose');
 
  //mongoose.connect('mongodb+srv://music:123@cluster2.dst31.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 //{ useNewUrlParser:true},(err)=>{
-  //  if (!err){ console.log('Congratulations your mongo is connected')}
+   // if (!err){ console.log('Congratulations your mongo is connected')}
     //else{console.log('Error in your DB Borges:' + err)}
 //});
 
-mongoose.connect('mongodb://localhost:27017/Musical_Work',
+mongoose.connect('mongodb+srv://music:123@cluster2.dst31.mongodb.net/Musical_Work?retryWrites=true&w=majority',
 { useNewUrlParser:true},(err)=>{
     if (!err){ console.log('Congratulations your mongo is connected')}
     else{console.log('Error in your DB Borges:' + err)}
 });
+
+
+
+//mongoose.connect('mongodb://localhost:27017/Musical_Work',
+//{ useNewUrlParser:true},(err)=>{
+  //  if (!err){ console.log('Congratulations your mongo is connected')}
+    //else{console.log('Error in your DB Borges:' + err)}
+//});
 
 require('./music.model');
