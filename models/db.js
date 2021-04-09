a624f255eb494d43f3c7f642dcd74f5ce37bff4b
Thenikde <thenilde.tb@hotmail.com>
2021-04-09 22:46:50 +0000

    chnageDB

--- models/db.js
+++ models/db.js
@@ -1,12 +1,7 @@
 const mongoose=require('mongoose');
 
- //mongoose.connect('mongodb+srv://music:123@cluster2.dst31.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-//{ useNewUrlParser:true},(err)=>{
-   // if (!err){ console.log('Congratulations your mongo is connected')}
-    //else{console.log('Error in your DB Borges:' + err)}
-//});
 
-mongoose.connect('mongodb+srv://music:123@cluster2.dst31.mongodb.net/Musical_Work?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://music:123@cluster0.vkjg1.mongodb.net/Musical_Work?retryWrites=true&w=majority',
 { useNewUrlParser:true},(err)=>{
     if (!err){ console.log('Congratulations your mongo is connected')}
     else{console.log('Error in your DB Borges:' + err)}
