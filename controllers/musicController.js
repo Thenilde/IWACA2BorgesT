b3fad6447dbbfb7e91ebebff1b65ce3cbabafa19
Thenikde <thenilde.tb@hotmail.com>
2021-04-09 01:19:36 +0000

    changes13

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -17,8 +17,24 @@ router.post('/',(req, res)=>{
 
 function insertRecord(req,res){
  var music=new Music();
- music.Instrument
+ music.Instrument=req.body.Instrument;
+ music.Price=req.body.Price;
+ music.Color=req.body.Color;
+ music.Year=req.body.Year;
+ music.Comments=req.body.Comments;
+ music.save((err,doc)=>{
+  if(err)
+     res.redirect('music/list');
+ else{
+     console.log('Error diring recording insert:' ,err);
+ }
+
+ });
 
 }
+
+router.get('/list',(req,res)=>{
+    res.json('from list');
+});
 
 module.exports=router;
